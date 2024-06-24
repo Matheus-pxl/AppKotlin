7fe5da976ad0d6e5b8dafd240f4a4ada99f6467e
Matheus <matheushellf@gmail.com>
2024-06-24 20:47:49 -0300

    atributo senha

--- app/src/main/java/com/example/loginapp/model/Usuario.kt
+++ app/src/main/java/com/example/loginapp/model/Usuario.kt
@@ -10,8 +10,9 @@ import java.util.Date
  * Entidade que representa um usuário no banco de dados.
  * @property id Identificador único do usuário (chave primária).
  * @property nome Nome do usuário.
- * @property endereco Endereço do usuário.
+ * @property email Email do usuário.
  * @property nascimento Data de nascimento do usuário, convertida utilizando DateConverter.
+ * @property senha Senha do usuário.
  */
 @Entity
 class Usuario(
@@ -21,8 +22,11 @@ class Usuario(
     @ColumnInfo(name = "nome")
     var nome: String?,
 
-    @ColumnInfo(name = "endereço")
-    var endereco: String?,
+    @ColumnInfo(name = "email")
+    var email: String?,
+
+    @ColumnInfo(name = "senha")
+    var senha: String?,
 
     @TypeConverters(DateConverter::class)
     @ColumnInfo(name = "nascimento")
