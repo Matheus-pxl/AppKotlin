69b100b0ac1f8a3814c0abebd526b060f40e7d11
Econsoft <matheushellf@gmail.com>
2024-06-26 08:47:26 -0300

    layout de cadastro

---
+++ app/src/main/java/com/example/loginapp/ui/activity/CadastroFormularioActivity.kt
@@ -0,0 +1,16 @@
+package com.example.loginapp.ui.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.loginapp.R
+
+class CadastroFormularioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_formulario)
+
+    }
+}
